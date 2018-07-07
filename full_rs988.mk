6101e54d60e4302906ddd3b61db09d9d65356ee9
imasaru <evanmr4@gmail.com>
2018-07-07 02:00:11 -0500

    rs988: Correct device.mk inheritance comment
    Change-Id: I1bc1b73ee9fe53dc605db5118e55efac8f5cc6e3

--- full_rs988.mk
+++ full_rs988.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from h815 device
+# Inherit from rs988 device
 $(call inherit-product, device/lge/rs988/device.mk)
 
 # Set those variables here to overwrite the inherited values.
